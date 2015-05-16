20d1eecf42a9bfbd99b1166f1915ce749350775a
Yuri Feldman <yuri@tbqh.net>
2015-05-16 17:40:10 +1000

    UTFGrid and style properties parsing

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var tilelive = require("tilelive"),
-    server = require("express")(),
+    app = require("express")(),
     os = require('os'),
     port = 5044,
     path = require("path"),
-    fs = require("fs");
+    rw = require("rw"),
+    yaml = require("js-yaml");
 
 // TODO(yuri): Implement tilecache
 require("mbtiles").registerProtocols(tilelive);
@@ -22,51 +23,77 @@ var mbtiles = "mbtiles:///Users/arrayjam/code/tilelive_server/tiles/Geelong.mbti
 
 tilelive.info(mbtiles, function(err, tilejson) {
     tilejson.tiles = [ "http://" + os.hostname() + ":" + port + "/" + tilesName + "/{z}/{x}/{y}.png" ];
-    // TODO(yuri): UTFGrid
     tilejson.scheme = "xyz";
-    // TODO(yuri): Why is minzoom/maxzoom not being filled out properly?
-    // It's determined by source, not the style
-    tilejson.maxzoom = 18;
+
+    // console.log(tilejson);
+
+    var xml = rw.readFileSync(path.join(process.cwd(), "geelong_roofs_style.tm2", "project.xml"), "utf8");
+    var yml = yaml.safeLoad(rw.readFileSync(path.join(process.cwd(), "geelong_roofs_style.tm2", "project.yml"), "utf8"));
+    console.log(yml);
     console.log(tilejson);
 
+    tilejson.maxzoom = yml.maxzoom;
+    tilejson.minzoom = yml.minzoom;
+    tilejson.bounds = yml.bounds;
+    tilejson.center = yml.center;
+    tilejson.name = yml.name;
+    tilejson.description = yml.description;
+    var enableUTFGrid = yml.interactivity_layer !== '';
 
-    var tm2Path = path.join(process.cwd(), "geelong_roofs_style.tm2", "project.xml");
+    if(enableUTFGrid) {
+        tilejson.grids = [ "http://" + os.hostname() + ":" + port + "/" + tilesName + "/{z}/{x}/{y}.grid.json" ];
+    }
+
+
     // TODO(yuri): Differentiate between vector tiles and normal mbtiles
-    fs.readFile(tm2Path, 'utf8', function(err, xml) {
-        // TODO(yuri): Is it really necessary to construct our own Backend here?
-        // Can we use a different constructor for Vector?
-        new Vector.Backend({
-            uri: mbtiles,
-        }, function(err, backend) {
-            // console.log("backend", backend);
-            new Vector({
-                xml: xml,
-                backend: backend
-            }, function(err, tilestore) {
-                // console.log(tilestore._map.fonts());
-                // tilestore.getTile();
-                // TODO(yuri): Handle both normal DPI and @2x
-                server.get("/" + tilesName + "/:z/:x/:y.png", function(req, res) {
-                    tilestore.getTile(req.params.z, req.params.x, req.params.y, function(err, tile, headers) {
-                        console.log(arguments);
-                        if (!err) {
-                            res.set(headers);
-                            res.send(tile);
-                        } else {
-                            res.status(404).send("Tile rendering error: " + err + "\n");
-                        }
-
-                    });
+    // NOTE(yuri): We create our own Backend and pass it to Vector, because by default
+    // the Vector constructor uses the tiles listed in the xml ahead of any other source
+    // you give it. So the tileset that's requested is the tm2source from Mapbox Studio,
+    // whereas we want to serve mbtiles. Unless we want to use tilelive-tm2source.
+    new Vector.Backend({
+        uri: mbtiles,
+    }, function(err, backend) {
+        new Vector({
+            xml: xml,
+            backend: backend
+        }, function(err, tilestore) {
+            // console.log(tilestore);
+            // TODO(yuri): Figure out a better way to express (pun intended) this path
+            app.get("/" + tilesName + "/:z/:x/:y@?2?x?.png", function(req, res) {
+                tilestore.getTile(req.params.z, req.params.x, req.params.y, function(err, tile, headers) {
+                    console.log(arguments);
+                    if (!err) {
+                        res.set(headers);
+                        return res.send(tile);
+                    } else {
+                        return res.status(404).send("Tile rendering error: " + err + "\n");
+                    }
                 });
             });
+
+            if(enableUTFGrid) {
+                app.get("/" + tilesName + "/:z/:x/:y.grid.json", function(req, res) {
+                    tilestore.getGrid(req.params.z, req.params.x, req.params.y, function(err, grid, headers) {
+                        console.log(arguments);
+                        if (!err) {
+                            res.header("Access-Control-Allow-Origin", "*");
+                            res.header("Access-Control-Allow-Headers", "X-Requested-With");
+                            return res.send(grid);
+                        } else {
+                            return res.status(404).send("Grid rendering error: " + err + "\n");
+                        }
+                    });
+                });
+            }
         });
     });
 
-    server.get("/" + tilesName + ".json", function(req, res) {
+    app.get("/" + tilesName + ".json", function(req, res) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "X-Requested-With");
         return res.json(tilejson);
     });
+
 
 
     // Vector.xray({uri: mbtiles}, function(err, tilestore) {
@@ -129,7 +156,7 @@ tilelive.info(mbtiles, function(err, tilejson) {
     // });
 });
 
-server.listen(port);
+app.listen(port);
 
 // if (applicationPrefix !== "" && applicationPrefix[applicationPrefix.length - 1] !== "/") {
 //   console.log("Application Prefix must be empty or have a trailing slash: currently it is '" + applicationPrefix + "'.");
